1f6dae40ab2b7cd96f1fb7ede38f33611356cb5e
Guilherme Garcia <garciag@alunos.utfpr.edu.br>
2025-09-12 15:20:51 -0300

    feito a parte do mux e das operações

--- Lab1/ULA.vhd
+++ Lab1/ULA.vhd
@@ -1,2 +1,31 @@
 library ieee;
-use ieee.std_logic_1164.all; meu nome é guilherme
+use ieee.std_logic_1664.all;
+use ieee.numeric_std_all;
+
+entity ULA is
+    port(
+        A,B : in unsigned(15 downto 0);
+        sel_op : in unsigned(1 downto 0);
+        S : out unsigned(15 downto 0)
+    );
+
+end entity;
+
+
+architecture A_ULA of ULA is
+    signal op1,op2,op3,op4,result : std_logic_vector(15 downto 0);
+
+    begin
+
+        op1 <= A + B;
+        op2 <= A - B;
+        op3 <= A nand B;
+        op4 <= A xor B;
+
+        result <= op1 when sel_op = "00" else
+                op2 when sel_op = "01"   else
+                op3 when sel_op = "10"   else
+                op4 when sel_op = "11"   else
+                "000000000000000";
+
+    end architecture;
